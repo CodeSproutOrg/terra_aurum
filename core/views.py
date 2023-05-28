004b960cacd2eca65d89368cc41930a3cdcfdda0
vladibuyanov <vladibuyanov@gmail.com>
2023-05-28 19:17:41 +0200

    1. Added new pages 2. Added pages header

--- core/views.py
+++ core/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
 
+template_folder = 'pages'
 menu = {
-    'O NÁS': '/',
-    'PODUJATIA': '/',
+    'O NÁS': '/about-us',
+    'PODUJATIA': '/events',
     'NAŠE AKTIVITY': '',
     'KONTAKT': '',
-    'DOKUMENTY': ''
+    'DOKUMENTY': '/documents'
 }
 
+
 def index(request):
-    template = 'pages/index.html'
-    data = {"title": "Voices of Hope", "menu": menu}
+    template = f'{template_folder}/index.html'
+    data = {"title": "TERRA-AURUM", "menu": menu}
     return render(request, template, context=data)
 
+
+def about_us(request):
+    template = f'{template_folder}/about-us.html'
+    data = {"title": "O NÁS", "menu": menu}
+    return render(request, template, context=data)
+
+
+def events(request):
+    template = f'{template_folder}/events.html'
+    data = {"title": "PODUJATIA", "menu": menu}
+    return render(request, template, context=data)
+
+
+def documents(request):
+    template = f'{template_folder}/documents.html'
+    data = {"title": "DOKUMENTY", "menu": menu}
+    return render(request, template, context=data)
--- terra_aurum/urls.py
+++ terra_aurum/urls.py
@@ -5,6 +5,9 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about-us', views.about_us, name='about-us'),
+    path('events', views.events, name='events'),
+    path('documents', views.documents, name='documents'),
 
     path('admin/', admin.site.urls),
 ]
