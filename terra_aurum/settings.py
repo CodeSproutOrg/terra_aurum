3d928ab0d64ace821b8602c505110d84a64e34e8
vladibuyanov <vladibuyanov@gmail.com>
2023-08-18 12:51:24 +0200

    Css fixes
    1. Fixed css code for header, footer
    2. Small fixes

--- terra_aurum/settings.py
+++ terra_aurum/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('KEY')
+SECRET_KEY = os.getenv('KEY', 'development-key')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
