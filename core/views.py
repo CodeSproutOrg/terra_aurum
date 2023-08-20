3d9f15cc29cd650b73d23254e45d4ace74e637fb
vladibuyanov <vladibuyanov@gmail.com>
2023-08-20 17:40:20 +0200

    Event model:
    - Upcoming Events have been added to the homepage

--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
 
+from django.db.models import Q
 from core.models import Event
+
 
 template_folder = 'pages'
 menu = {
     'O NÁS': '/about-us',
     'PODUJATIA': '/events',
-    'NAŠE AKTIVITY': '',
     'KONTAKT': '',
     'DOKUMENTY': '/documents'
 }
@@ -15,22 +18,22 @@ menu = {
 def index(request):
     template = f'{template_folder}/index.html'
     data = {"title": "TERRA-AURUM", "menu": menu}
+
+    current_date = datetime.now()
+    three_months_later = current_date + timedelta(days=90)
+
+    upcoming_events = Event.objects.filter(
+        Q(time__gte=current_date) & Q(time__lte=three_months_later)
+    )
+
+    data['events'] = upcoming_events
+
     return render(request, template, context=data)
 
 
 def about_us(request):
     template = f'{template_folder}/about-us.html'
     data = {"title": "O NÁS", "menu": menu}
-    return render(request, template, context=data)
-
-
-def events(request):
-    template = f'{template_folder}/events.html'
-    data = {"title": "PODUJATIA", "menu": menu}
-
-    all_events = Event.objects.all()
-    data['all_events'] = all_events
-
     return render(request, template, context=data)
 
 
@@ -43,6 +46,16 @@ def event(request, slug):
         "menu": menu,
         'event': this_event
     }
+    return render(request, template, context=data)
+
+
+def events(request):
+    template = f'{template_folder}/events.html'
+    data = {"title": "PODUJATIA", "menu": menu}
+
+    all_events = Event.objects.all()
+    data['all_events'] = all_events
+
     return render(request, template, context=data)
 
 
