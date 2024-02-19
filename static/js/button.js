fd0ded94c151f92218ff756a19719b9e7012b1b2
Vladi <vladibuyanov@gmail.com>
2024-02-19 14:07:43 +0100

    WIP Refactor CSS/HTML code + fix for errors in mobile menu layout
    Closes #3

--- static/js/button.js
+++ static/js/button.js
@@ -1,8 +1,16 @@
 let MobNavButton = document.getElementById('menu-button');
+let navbar = document.getElementById('nav-bar');
 let header = document.getElementById('header');
 
 MobNavButton.addEventListener('click', OpenMenu);
+document.addEventListener('click', ClosedMenu);
 
 function OpenMenu() {
-    header.classList.toggle("open");
+    event.stopPropagation();
+    navbar.classList.toggle("open");
+    MobNavButton.classList.toggle("closed");
+}
+function ClosedMenu() {
+    navbar.classList.toggle("open");
+    MobNavButton.classList.toggle("closed");
 }
