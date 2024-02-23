53dbbc193a58634a67c88a0cc4b767d4e7dc55dc
Vladi <vladibuyanov@gmail.com>
2024-02-23 13:44:27 +0100

    Carousel for events on main page, temporary solution

--- static/js/button.js
+++ static/js/button.js
@@ -5,6 +5,28 @@ let header = document.getElementById('header');
 MobNavButton.addEventListener('click', OpenMenu);
 document.addEventListener('click', ClosedMenu);
 
+
+let events = document.getElementsByClassName('main-event');
+let totalEvents = events.length;
+let currentIndex = 0;
+
+setInterval(() => {
+    try {
+        if (currentIndex != 0 & currentIndex < totalEvents) {
+            events[currentIndex - 1].style.transform = `translateX(${currentIndex * 100}%)`;
+            events[currentIndex].style.transform = `translateX(-${currentIndex * 100}%)`;
+        } else if (currentIndex == totalEvents) {
+            events[currentIndex - 1].style.transform = `translateX(${currentIndex * 100}%)`;
+            currentIndex = -1
+        }
+        currentIndex = (currentIndex + 1);
+    } catch (error) {
+        console.log("Content not exist" + error);
+    };
+}, 10000);
+
+
+
 function OpenMenu() {
     event.stopPropagation();
     navbar.classList.toggle("open");
